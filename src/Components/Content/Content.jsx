85e596f5bd1db7392c3349dc38fd779e00902c30
Artem Klokov <clockytema@gmail.com>
2024-10-15 14:06:14 +0300

    first commit

---
+++ src/Components/Content/Content.jsx
@@ -0,0 +1,11 @@
+import classes from './Content.module.css'
+
+const Content = () => {
+    return (
+      <div>
+        Content
+      </div>
+    );
+  }
+
+export default Content;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import classes from './Header.module.css'
+
+const Header = () => {
+    return (
+      <div /*className={classes.img}*/>
+        Header
+      </div>
+    );
+  }
+
+export default Header;
